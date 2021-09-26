31c7f8bcc8282b1687ef33ef27a958e15d33e868
Chiharu Miya <miya.chiharu@gmail.com>
2021-09-26 18:02:02 +0900

    WebSocket部分の実装

--- webSocketServer/src/main/java/com/example/webSocketServer/api/SampleRestController.java
+++ webSocketServer/src/main/java/com/example/webSocketServer/api/SampleRestController.java
@@ -1,14 +1,23 @@
 package com.example.webSocketServer.api;
 
+import com.example.webSocketServer.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 @RestController
-@RequestMapping("/")
 public class SampleRestController {
-    @GetMapping
+    @GetMapping("/hello")
     String getHello() {
         return "Hello World";
+    }
+
+    @MessageMapping("/message")
+    @SendTo("/receive/message")
+    public Message send(Message message) throws Exception {
+        Thread.sleep(1000);
+        return new Message(HtmlUtils.htmlEscape(message.getName()), HtmlUtils.htmlEscape(message.getStatement()));
     }
 }
---
+++ webSocketServer/src/main/java/com/example/webSocketServer/model/Message.java
@@ -0,0 +1,14 @@
+package com.example.webSocketServer.model;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private String name;
+    private String statement;
+
+    public Message(String name, String statement) {
+        this.name = name;
+        this.statement = statement;
+    }
+}
